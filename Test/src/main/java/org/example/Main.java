d3b617e95650a4a81bb7a6fba2e5323c67c61547
DOnstor <253410@mail.ru>
2023-03-08 13:00:07 +0300

    1`

--- Test/src/main/java/org/example/Main.java
+++ Test/src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        hello();
+
+
+    }
+    public static void hello(){
         System.out.println("Hello world!");
     }
+
 }
