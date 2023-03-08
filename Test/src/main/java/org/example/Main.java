11bc490f11b326c0814de5e4566c1147fca84e88
DOnstor <253410@mail.ru>
2023-03-08 13:32:49 +0300

    2

--- Test/src/main/java/org/example/Main.java
+++ Test/src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
         hello();
-
+sum();
 
     }
     public static void hello(){
         System.out.println("Hello world!");
     }
+    public static void sum() {
+        Point p = new Point(2,4);
+        System.out.println(p.sum());
 
+    }
 }
