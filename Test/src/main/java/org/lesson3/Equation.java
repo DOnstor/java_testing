db1b4e1bace90c3338a10b1e448f8b6afdb8db9d
DOnstor <253410@mail.ru>
2023-03-11 15:42:55 +0300

    L3.2

--- Test/src/main/java/org/lesson3/Equation.java
+++ Test/src/main/java/org/lesson3/Equation.java
@@ -1,15 +1,26 @@
 package org.lesson3;
 
 public class Equation {
-    private int n;
+    private final int n;
 
     public Equation(double a, double b, double c) {
 
         double d = b * b - 4 * a * c;
-        if (d > 0) {
-            n = 2;
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else {
+                    n = 0;
+                }
+            } else {
+                n = 1;
+            } ;
+
         } else {
-            if (d == 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
                 n = 1;
             } else {
                 n = 0;
--- Test/src/test/java/ru/lesson3/EquationTests.java
+++ Test/src/test/java/ru/lesson3/EquationTests.java
@@ -20,4 +20,20 @@ public class EquationTests {
         Equation e = new Equation(1,5,6);
         Assert.assertEquals(e.rootNumber(),2);
     }
+    @Test
+    public void test3(){
+        Equation e = new Equation(0,1,1);
+        Assert.assertEquals(e.rootNumber(),1);
+
+    }
+    @Test
+    public void test4(){
+        Equation e = new Equation(0,0,1);
+        Assert.assertEquals(e.rootNumber(),0);
+    }
+    @Test
+    public void testZ(){
+        Equation e = new Equation(0,0,0);
+        Assert.assertEquals(e.rootNumber(),-1);
+    }
 }
