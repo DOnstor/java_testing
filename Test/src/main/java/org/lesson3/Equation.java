38955c8ebba4bbf0b143ff8a501f8a309ac01aa5
DOnstor <253410@mail.ru>
2023-03-11 15:29:24 +0300

    L3.1

---
+++ Test/src/main/java/org/lesson3/Equation.java
@@ -0,0 +1,23 @@
+package org.lesson3;
+
+public class Equation {
+    private int n;
+
+    public Equation(double a, double b, double c) {
+
+        double d = b * b - 4 * a * c;
+        if (d > 0) {
+            n = 2;
+        } else {
+            if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
+        }
+    }
+
+    public int rootNumber() {
+        return n;
+    }
+}
